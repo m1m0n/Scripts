22dd7a41498ce330818d36f0cb59251cbb7a536a
Ahmed Moustafa <m1m0n@localhost.localdomain>
2021-01-30 21:11:47 +0200

    edit the script to use threads

--- rename/rename.py
+++ rename/rename.py
@@ -1,23 +1,29 @@
 import os
 from mp3_tagger import MP3File
+import threading 
 
-files = os.listdir(".")
+def change_name(dir):
+	for file in files:
 
-for file in files:
+		if file.endswith(".mp3"):
+			
+			mp3 = MP3File(file)
+			tags = mp3.get_tags()
+			ac_name = tags["ID3TagV1"]["song"]
+			new_name = file.replace(".mp3", "") +"-"+ ac_name
 
-	if file.endswith(".mp3"):
-		
-		mp3 = MP3File(file)
-		tags = mp3.get_tags()
-		ac_name = tags["ID3TagV1"]["song"]
-		new_name = file.replace(".mp3", "") +"-"+ ac_name
-
-		if ac_name in file:
-			print(file + " is already Renamed")
+			if ac_name in file:
+				print(file + " is already Renamed")
+			else:
+				os.rename("./" + file , "./" + new_name + ".mp3")
+				print("[+] File Name Changed From " + file + " to " + new_name + ".mp3")
 		else:
-			os.rename("./" + file , "./" + new_name + ".mp3")
-			print("[+] File Name Changed From " + file + " to " + new_name + ".mp3")
-	else:
-		pass
+			pass
+
+if __name__ == "__main__":
 		
-print("All File Names Changed Successfully")
+	files = os.listdir(".")
+	t1 = threading.Thread(target=change_name, args=(files,))
+	t1.start()
+	t1.join()
+	print("All File Names Changed Successfully")
